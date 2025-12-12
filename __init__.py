3f79b258477ce959fd2f04a8329a8897475c4904
wartwix <n.sadra1990@gmail.com>
2025-12-12 09:39:46 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -5,7 +5,7 @@ from datetime import datetime
 from urllib.request import urlopen
 import sqlite3
 
-# CORRECTION : Utiliser la variable spéciale __name__ (double underscore)
+# Initialisation de l'application Flask
 app = Flask(__name__)
 
 # Route /contact/
@@ -16,24 +16,30 @@ def MaPremiereAPI():
 # Route /tawarano/
 @app.route('/tawarano/')
 def meteo():
-    # URL corrigée de tout éventuel &amp;
+    # Appel de l'API OpenWeatherMap, extraction et conversion K -> °C
     response = urlopen('https://samples.openweathermap.org/data/2.5/forecast?lat=0&lon=0&appid=xxx')
     raw_content = response.read()
     json_content = json.loads(raw_content.decode('utf-8'))
     results = []
     for list_element in json_content.get('list', []):
         dt_value = list_element.get('dt')
-        # Conversion de Kelvin en °c 
         temp_day_value = list_element.get('main', {}).get('temp') - 273.15 
         results.append({'Jour': dt_value, 'temp': temp_day_value})
     return jsonify(results=results)
 
-# Route /
+# =========================================================
+# AJOUT DE LA NOUVELLE ROUTE : /rapport/ (Exercice 3 Bis)
+# =========================================================
+@app.route("/rapport/")
+def mongraphique():
+    # Flask cherche automatiquement "graphique.html" dans le dossier 'templates'
+    return render_template("graphique.html")
+
+# Route / (Page d'accueil)
 @app.route('/')
 def hello_world():
     return render_template('hello.html')
 
-# CORRECTION : Utiliser la variable spéciale __name__ (double underscore)
+# Bloc d'exécution principal
 if __name__ == "__main__":
   app.run(debug=True)
-    
